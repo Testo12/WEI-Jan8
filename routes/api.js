b3377965b2100327ffb67ce6bfc0af47f807bfac
RyuTribal <ivan.sedelkin9@gmail.com>
2019-01-08 16:36:22 +0100

    Added a backend add function for members and skills

--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,8 @@ var controller = require('../controllers/api');
 
 router.post('/login', controller.login);
 router.post('/register', controller.register);
+router.post('/member', controller.member);
+router.post('/skill', controller.skill);
 router.get('/home', controller.home);
 router.get('/homepage', controller.homepage);
 router.get('/cue', controller.cue);
