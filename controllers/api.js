b355fafead5902be6ad2014e42d4815b2fc58352
RyuTribal <ivan.sedelkin9@gmail.com>
2019-01-08 15:46:16 +0100

    Added frontend and backend functions for login

--- controllers/api.js
+++ controllers/api.js
@@ -2,8 +2,25 @@ var Users = require('../models/user');
 var Admin = require('../models/admin');
 var passport = require('passport');
 var jwt = require('jwt-simple');
+var config = require('../config/database');
 var passed = false;
 var token;
+
+exports.register = function(req,res){
+    var register = new Admin({
+        userName: req.body.userName,
+        password: req.body.password
+    });
+
+    
+    register.save(function(error){
+        //obs hantera error
+        if (error){
+            return next(error);
+        }
+        res.send('user created');
+    });
+};
 
 exports.list = function(req,res){
     Users.find(function(err, users){
@@ -44,41 +61,41 @@ exports.wei = function(req,res){
 };
 
 exports.login = function(req,res){
-    User.findOne({
+    Admin.findOne({
         userName: req.body.userName.toLowerCase()
     }, function(err, user){
         if (err) throw err;
 
         if(!user){
-            return res.send('error');
+            return res.send('error no users found');
             
             
         } else{
+            console.log(user);
             user.comparePassword(req.body.password.toLowerCase(), function(err, isMatch){
                 if (isMatch && !err){
                     var createdToken = jwt.encode(user, config.secret);
                     token = 'Bearer '+createdToken;
-                    //res.json({success: true, token: 'Bearer ' + token});
+                    //res.json({success: true, token: 'Bearer ' + createdToken});
                     //res.cookie('auth', 'Bearer ' + token);
                     res.send('Bearer '+createdToken);
                     
                     
                 }else{
-                    return res.send('error');
+                    return res.send('error, wrong password');
                 }
             })
         }
     });
 };
 
-exports.home = passport.authenticate('jwt', {session: false}), function (req, res){
+exports.home = function (req, res){
+    passport.authenticate('jwt', {session: false});
     var createdToken = getToken(req.headers);
     if(createdToken){
         var decoded = jwt.decode(createdToken, config.secret);
-        firstName = decoded.firstName;
-        lastName = decoded.lastName;
-        User.findOne({
-            email: decoded.email
+        Admin.findOne({
+            userName: decoded.userName
         }, function(err, user){
             if(err) throw err;
 
@@ -86,7 +103,7 @@ exports.home = passport.authenticate('jwt', {session: false}), function (req, re
                 return res.status(403).send({success: false, msg: 'User not found'});
             }else {
                 passed = true;
-                return res.send('../../api/map/home');
+                return res.send('../../api/homepage');
 
             }
         })
@@ -95,3 +112,27 @@ exports.home = passport.authenticate('jwt', {session: false}), function (req, re
     }
 
 };
+
+exports.homepage = function(req,res){
+    if(passed == true){
+        res.render('home');
+    }
+    else{
+        res.redirect('/');
+    }
+};
+
+getToken = function(headers){
+    if(headers && headers.authorization){
+        var parted = headers.authorization.split(' ');
+        if (parted.length === 2){
+            return parted[1];
+        } else {
+            return null;
+        }
+    }else {
+        return null;
+    }
+}
+
+require('../config/passport')(passport);
--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,9 @@ var router = express.Router();
 var controller = require('../controllers/api');
 
 router.post('/login', controller.login);
+router.post('/register', controller.register);
+router.get('/home', controller.home);
+router.get('/homepage', controller.homepage);
 router.get('/cue', controller.cue);
 router.get('/wei', controller.wei);
 module.exports = router;
